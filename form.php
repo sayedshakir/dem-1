d9302b883d84361bbac32a35110b5270aa8a451d
SAYED RAHEMATULLAH SHAKIR <46511486+sayedshakir@users.noreply.github.com>
2021-03-31 11:08:50 +0530

    Update form.php

--- form.php
+++ form.php
@@ -1,12 +1,11 @@
 <?php
-if(isset($_POST['submit'])){
-	$num1=$_POST['num1'];
-	$num2=$_POST['num2'];
+
+	$num1=$_GET['num1'];
+	$num2=$_GET['num2'];
 	echo $num1+$num2;
-}
 
 ?>
-<form method="post">
+<form method="get">
 	<input type="textbox" name="num1" placeholder="Num1"/><br/><br/>
 	<input type="textbox" name="num2" placeholder="Num2"/><br/><br/>
 	<input type="submit" name="submit"/>
